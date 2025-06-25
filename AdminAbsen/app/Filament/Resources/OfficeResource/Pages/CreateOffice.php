46a481efa3d131818f920664a52e1c36802eea45
luhatfimhalaf <luhatfim.halaf1@gmail.com>
2025-06-25 23:52:34 +0700

    UPDATE FITUR OFFICE

--- AdminAbsen/app/Filament/Resources/OfficeResource/Pages/CreateOffice.php
+++ AdminAbsen/app/Filament/Resources/OfficeResource/Pages/CreateOffice.php
@@ -11,15 +11,10 @@ class CreateOffice extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        // Ambil latitude dari location[lat] atau latitude, default ke -6.200000
-        $data['latitude'] = (float) ($data['location']['lat'] ?? ($data['latitude'] ?? -6.9431000));
-        // Ambil longitude dari location[lng] atau longitude, default ke 106.816666
-        $data['longitude'] = (float) ($data['location']['lng'] ?? ($data['longitude'] ?? 107.5851494));
-        // Pastiin radius ada, default ke 100 kalau kosong
+        $data['latitude'] = -6.9431000;
+        $data['longitude'] = 107.5851494;
         $data['radius'] = (float) ($data['radius'] ?? 100);
-        // Hapus field location biar nggak disimpen
         unset($data['location']);
-
         return $data;
     }
 }
